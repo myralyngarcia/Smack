19dee0f13ddf144738e43cbb1cbeb891b0847cc1
Myralyn Garcia <mygarcia@bbmtek.com>
2018-02-06 22:43:50 -0500

    broadcast receiver

--- app/src/main/java/com/example/myralyn/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/example/myralyn/smack/Controller/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.example.myralyn.smack.Controller
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.content.res.Resources
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.example.myralyn.smack.R
+import com.example.myralyn.smack.Services.AuthService
+import com.example.myralyn.smack.Services.UserDataService
+import com.example.myralyn.smack.Services.UserDataService.name
+import com.example.myralyn.smack.Utilities.BROADCAST_DATA_CHANGED
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity(){
 
@@ -22,7 +32,23 @@ class MainActivity : AppCompatActivity(){
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+        //registerReceiver needs a receiver and IntentFilter so that it will not receive all intent
+        LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver, IntentFilter(BROADCAST_DATA_CHANGED))
 
+    }
+    //create a broadcastreceiver object to be passed as receiver to the broadcastManager
+    private val userDataChangeReceiver = object: BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent?) {
+            //update navHeader ui
+            if (AuthService.isLoggedIn){
+                loginBtnNavHeader.text ="logout"
+                usernameNavHeader.text = UserDataService.name
+                userEmailNavHeader.text = UserDataService.email
+                val resourceId = resources.getIdentifier(UserDataService.avatarName, "drawable", packageName)
+                userimageNavHeader.setImageResource(resourceId)
+                userimageNavHeader.setBackgroundColor(UserDataService.returnAvatarColor(UserDataService.avatarColor))
+            }
+        }
     }
 
     override fun onBackPressed() {
--- app/src/main/java/com/example/myralyn/smack/Services/UserDataService.kt
+++ app/src/main/java/com/example/myralyn/smack/Services/UserDataService.kt
@@ -1,5 +1,10 @@
 package com.example.myralyn.smack.Services
 
+import android.graphics.Color
+import com.example.myralyn.smack.R.id.basic
+import com.example.myralyn.smack.R.id.loginBtnNavHeader
+import java.util.*
+
 /**
  * Created by myralyn on 04/02/18.
  */
@@ -10,4 +15,26 @@ object UserDataService {
     var email = ""
     var name = ""
 
+    //the component is the RGB and we return Int coz in android colors are actually integers
+    fun returnAvatarColor (components: String): Int{
+        //this is the string array that we got for the user avatarColor in the database
+        //[0.13333333333333333, 0.756862274450980392, 0.7215686274509804, 1]
+        val stripColor = components
+                .replace("[", "")
+                .replace("]", "")
+                .replace(",", "")
+        //scanner will start at beginning of string and pick up the first double value, next double value, next double value
+        var r = 0
+        var g = 0
+        var b = 0
+        //stripColor is: 0.13333333333333333 0.756862274450980392 0.7215686274509804
+        var scanner = Scanner(stripColor)
+        if (scanner.hasNext()){
+            r = (scanner.nextDouble() * 255).toInt()
+            g = (scanner.nextDouble() * 255).toInt()
+            b = (scanner.nextDouble() * 255).toInt()
+
+        }
+        return Color.rgb(r, g, b)//returns an integer for the rgb
+    }
 }
