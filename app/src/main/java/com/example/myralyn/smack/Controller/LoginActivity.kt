f6d7fcbcabe794d844eae3b52c64038f13ff4a8d
Myralyn Garcia <mygarcia@bbmtek.com>
2018-02-13 22:00:24 -0500

    login activity

--- app/src/main/java/com/example/myralyn/smack/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/myralyn/smack/Controller/LoginActivity.kt
@@ -1,34 +1,53 @@
 package com.example.myralyn.smack.Controller
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import com.example.myralyn.smack.R
 import com.example.myralyn.smack.Services.AuthService
 import kotlinx.android.synthetic.main.activity_login.*
+import java.util.*
+
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loginSpinner.visibility = View.INVISIBLE
     }
 
     fun loginLoginBtnClicked (view: View){
+
+        loginSpinner(true)
         val email = loginEmailText.text.toString()
         val password = loginPasswordText.text.toString()
-
-        AuthService.loginUser(this, email, password){loginSuccess ->
-            if(loginSuccess){
-                AuthService.findUserByEmail(this){findSuccess ->
-                    if (findSuccess){
-                        finish()
+        hideKeyboard()
+        if (email.isNotEmpty() && password.isNotEmpty()){
+            AuthService.loginUser(this, email, password){loginSuccess ->
+                if(loginSuccess){
+                    AuthService.findUserByEmail(this){findSuccess ->
+                        if (findSuccess){
+                            loginSpinner(false)
+                            finish()
+                        }
+                        else{
+                            errorToast()}
                     }
-                }
-            }
+                }else{
+                    errorToast()}
 
+            }
+        }else{
+            Toast.makeText(this, "Please make sure email and password are not empty", Toast.LENGTH_LONG).show()
+            loginSpinner(false)
         }
+
+
 
     }
     //this is when SIGN UP HERE is clicked, we don't go back and click the button again so we finish
@@ -37,6 +56,30 @@ class LoginActivity : AppCompatActivity() {
         startActivity(createUserIntent)
         finish()
 
+    }
+
+    fun errorToast(){
+        //Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show()
+        loginSpinner(false)
+    }
+
+    fun loginSpinner(enable: Boolean) {
+        if (enable) {
+            loginSpinner.visibility = View.VISIBLE
+        } else {
+            loginSpinner.visibility = View.INVISIBLE
+        }
+        loginLoginBtn.isEnabled = !enable
+        loginCreateUserBtn.isEnabled = !enable
+    }
+
+    fun hideKeyboard (){
+        var inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        if (inputManager.isAcceptingText){
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+        }
     }
 
 }
